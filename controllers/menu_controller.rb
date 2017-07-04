d67616ed0ff76cedac8bd8ffeee5be1a15eecd6e
Renee Bain <rbain02@gmail.com>
2017-07-04 11:20:41 -0400

    checkpoint8: completed

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -56,7 +56,10 @@ class MenuController
     case selection
     when "n"
     when "d"
+      delete_enty(entry)
     when "e"
+      edit_entry(entry)
+      entry_submenu(entry)
     when "m"
       system "clear"
       main_menu
@@ -82,7 +85,7 @@ class MenuController
     puts "New AddressBloc Entry"
     print "Name: "
     name = gets.chomp
-    print "Phone numer: "
+    print "Phone number: "
     phone = gets.chomp
     print "Email: "
     email = gets.chomp
@@ -93,5 +96,84 @@ class MenuController
   end
 
   def read_csv
+    print "Enter CSV file to import: "
+    file_name = gets.chomp
+
+    if file_name.empty?
+      system "clear"
+      puts "No CSV file read"
+      main_menu
+    end
+
+    begin
+      entry_count = address_book.import_from_csv(file_name).count
+      puts "#{entry_count} new entries added from #{file_name}"
+    rescue
+      puts "#{file_name} is not a valid file, please enter the name of a valid CSV file"
+      read_csv
   end
+
+  def search_submenu(entry)
+    puts "\nd - delete entry"
+    puts "e - edit entry"
+    puts "m - return to main menu"
+    selection = gets.chomp
+
+    case selection
+    when "d"
+      system "clear"
+      delete_enty(entry)
+      main_menu
+    when "e"
+      system "clear"
+      edit_entry(entry)
+      main_menu
+    when "m"
+      system "clear"
+      main_menu
+    else
+      system "clear"
+      puts "#{selection} is not a valid input"
+      puts entry.to_s
+      search_submenu(entry)
+    end
+  end
+
+  def search_entries
+    print "Search by name: "
+    name = gets.chomp
+    match = address_book.binary_search(name)
+    system "clear"
+
+    if match
+      puts match.to_s
+      search_submenu(match)
+    else
+      puts "No match found for #{name}"
+    end
+  end
+
+
+  def delete_enty(entry)
+    address_book.entries.delete(entry)
+    puts "#{entry.name} has been deleted"
+  end
+
+  def edit_entry(entry)
+    print "Updated name: "
+    name = gets.chomp
+    print "Updated phone number: "
+    phone_number = gets.chomp
+    print "Updated email: "
+    email = gets.chomp
+
+    entry.name = name if !name.empty?
+    entry.phone_number = phone_number if !phone_number.empty?
+    entry.email = email if !email.empty?
+    system "clear"
+
+    puts "Updated entry: "
+    puts entry
+  end
+
 end
